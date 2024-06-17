da3fd89fce82b1f3ef6f79a10bbbab190ca8465b
lucasgrf <lucasgabrielrf.contato@gmail.com>
2024-06-17 02:24:26 -0300

    commit inicial

---
+++ src/main/java/com/loiane/estruturadados/testes/Aula02.java
@@ -0,0 +1,10 @@
+package com.loiane.estruturadados.testes;
+
+import com.loiane.estruturadados.vetor.Vetor;
+
+public class Aula02 {
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(5);
+
+    }
+}
